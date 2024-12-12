ee03a7de07e36167b717874d8ec37b1d69d7a674
Adam Maas <adamjm1220@gmail.com>
2024-12-12 09:36:15 -0500

    ECS_Deployment

---
+++ modules/application/main.tf
@@ -0,0 +1,28 @@
+resource "aws_ecs_task_definition" "app" {
+  family                   = var.family_name
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = var.cpu
+  memory                   = var.memory
+  execution_role_arn       = var.execution_role_arn
+  task_role_arn            = var.task_role_arn
+  container_definitions    = file("container-definitions.json")
+}
+
+resource "aws_ecs_service" "app_service" {
+  name            = var.service_name
+  cluster         = aws_ecs_cluster.fargate_cluster.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = var.subnet_ids
+    security_groups = [aws_security_group.ecs_service.id]
+    assign_public_ip = false
+  }
+}
+
+output "ecs_service_arn" {
+  value = aws_ecs_service.app_service.id
+}
